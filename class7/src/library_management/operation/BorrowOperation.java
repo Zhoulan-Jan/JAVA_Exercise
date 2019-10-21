08c0c5ea8b4dba8693528a03e0ece643e97e2e0f
ZhouLan <1832311982@qq.com>
2019-10-21 17:27:19 +0800

    借书操作

--- class7/src/library_management/operation/BorrowOperation.java
+++ class7/src/library_management/operation/BorrowOperation.java
@@ -1,10 +1,29 @@
 package library_management.operation;
 
+import library_management.book.Book;
 import library_management.book.BookList;
+
+import java.util.Scanner;
 
 public class BorrowOperation implements IOperation {
     @Override
     public void work(BookList bookList) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入要查找的书籍名：");
+        String bookToFind = sc.next();
+        int i = 0;
+        for (; i < bookList.getSize(); i++) {
+            Book book = bookList.getBooks(i); //找到库中的第i本书
+            if (book.getTitle().equals(bookToFind)) {
+                if (book.isBorrowed()) {
+                    System.out.println("已借阅！");
+                } else {
+                    book.setBorrowed(false);
+                }
+            }
+        }
+        if (i >= bookList.getSize()) {
+            System.out.println("未找到该书");
+        }
     }
 }
