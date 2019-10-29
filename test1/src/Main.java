66ed724128262be6d21aac5a63676df823d28cfc
ZhouLan <1832311982@qq.com>
2019-10-29 09:43:07 +0800

    删除重复字符串

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -2,29 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //单组输入
 //        Scanner sc = new Scanner(System.in);
 //        String str = sc.next();
-//        String ans = null;
 //        int[] arr = new int[255];
 //        for (int i = 0; i < str.length(); i++) {
 //            arr[str.charAt(i)]++;
 //        }
-////        //字符数组全改为1
-////        for (int k = 0; k < arr.length; k++) {
-////            if (arr[k] != 0) {
-////                arr[k] = 1;
-////            }
-////        }
 //        for (int j = 0; j < str.length(); j++) {
 //            if (arr[str.charAt(j)] > 0) {
 //                System.out.print(str.charAt(j));
 //                arr[str.charAt(j)] = 0;
 //            }
 //        }
+        //多组输入
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNextInt()) {
-            String str = sc.hasNextInt();
-            String ans = null;
+        while (sc.hasNext()) {
+            String str = sc.next();
             int[] arr = new int[255];
             for (int i = 0; i < str.length(); i++) {
                 arr[str.charAt(i)]++;
