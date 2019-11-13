c514adce2cda41338e782c7209683744fa011659
ZhouLan <1832311982@qq.com>
2019-11-13 21:59:07 +0800

    排序

--- c16/src/Sort.java
+++ c16/src/Sort.java
@@ -4,11 +4,11 @@ public class Sort {
 
     //插入排序
     public static void insertSort(int[] arr) {
-        for (int bound = 0; bound < arr.length; bound++) {
+        for (int bound = 1; bound < arr.length; bound++) {
             int tmp = arr[bound];
             int cur = bound - 1;
             for (; cur >= 0; cur--) {
-                if (arr[bound] < arr[cur]) {
+                if (tmp < arr[cur]) {
                     arr[cur + 1] = arr[cur];
                 } else {
                     break;
@@ -19,11 +19,52 @@ public class Sort {
     }
 
     //希尔排序
+    public static void shellSort(int[] arr) {
+        int gap = arr.length;
+        while (gap > 1) {
+            shellSortGap(arr, gap);
+            gap /= 2;
+        }
+        shellSortGap(arr,1);
+    }
+    public static void shellSortGap(int[] arr, int gap) {
+        for (int bound = 0; bound < arr.length; bound++) {
+            int tmp = arr[bound];
+            int cur = bound - gap;
+            for (; cur >= 0; cur -= gap) {
+                if (tmp < arr[cur]) {
+                    arr[cur + gap] = arr[cur];
+                } else {
+                    break;
+                }
+            }
+            arr[cur + gap] = tmp;
+        }
+    }
 
     //选择排序
+    public static void selectSort(int[] arr) {
+        for (int bound = 0; bound < arr.length; bound++) {
+            int cur = bound + 1;
+            for (; cur < arr.length; cur++) {
+                if (arr[bound] > arr[cur]) {
+                    swap(arr, bound, cur);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int x, int y) {
+        int tmp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = tmp;
+    }
+
     public static void main(String[] args) {
         int[] array = {4,5,7,9,1,2,10,3,6};
-        insertSort(array);
+        //insertSort(array);
+        //shellSort(array);
+        selectSort(array);
         System.out.println(Arrays.toString(array));
     }
 }
