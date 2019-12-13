45fb7722bab062ebf2815e01813af19e9af38388
ZhouLan <1832311982@qq.com>
2019-12-13 13:27:26 +0800

    增加家具

--- c21/src/Add.java
+++ c21/src/Add.java
@@ -16,31 +16,15 @@ public class Add {
                 preparedStatement = connection.prepareStatement(sql);
                 preparedStatement.setString(1,name);
                 resultSet = preparedStatement.executeQuery();
-                if (resultSet != null) {
-                    //数据库无该家具 新增  //未能新增成功
-                    //写一个子函数试试
-//                    connection = DB.getConnection();
-//                    String sql2 = "insert into Furnitures( name, price, num,dates, style) values (?,?,?,?,?);";
-//                    preparedStatement = connection.prepareStatement(sql2);
-//                    preparedStatement.setString(1,name);
-//                    preparedStatement.setInt(2,price);
-//                    preparedStatement.setInt(3,num);
-//                    preparedStatement.setString(4,dates);
-//                    preparedStatement.setString(5,style);
-//                    System.out.println("insert success");
-                    sonAdd(name,price,num,dates,style);
-                } else {
-                    //数据库有该家具 修改数量
+                if (resultSet.next()) {
+                    //数据库中有该家具
                     int id = resultSet.getInt("id");
                     int nums = resultSet.getInt("num");
-                    System.out.println("id" + id);
-//                    connection =DB.getConnection();
-//                    String sql3 = "update furnitures set num = ? where id = ?;";
-//                    preparedStatement = connection.prepareStatement(sql3);
-//                    preparedStatement.setInt(1,nums + num);
-//                    preparedStatement.setInt(2,id);
-//                    System.out.println("num insert success");
-                    sonMod(id,nums+num);
+                    int x = nums+num; //nums是数据库已有的家具数量， num是新增的家具数量
+                    sonMod(id,x);
+                } else {
+                    //无该家具
+                    sonAdd(name,price,num,dates,style);
                 }
             } finally {
                 DB.close(connection,preparedStatement,resultSet);
@@ -48,32 +32,6 @@ public class Add {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //有的话 num+1
-        //无的话 新增
-//        try {
-//            try{
-//                //创建数据库连接
-//                connection = DB.getConnection();
-//                String sql = "insert into Furnitures( name, price, num,dates, style) values (?,?,?,?,?);";
-//                //操作命令对象执行sql语句，返回结果集resultSet  resultSet类似List<map<String,Object>> String 指表头
-//                preparedStatement = connection.prepareStatement(sql);
-//                preparedStatement.setString(1,name);
-//                preparedStatement.setInt(2,price);
-//                preparedStatement.setInt(3,num);
-//                preparedStatement.setString(4,dates);
-//                preparedStatement.setString(5,style);
-//
-//                int rows = preparedStatement.executeUpdate(); //指示受影响的行数
-//                if (rows > 0) {
-//                    System.out.println("add success");
-//                }
-//            } finally {
-//                //释放资源 反向释放
-//                DB.close(connection,preparedStatement,resultSet);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
     }
 
     //新增子函数
