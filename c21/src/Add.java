d115beaae520daec0b776f082191836e252095f7
ZhouLan <1832311982@qq.com>
2019-12-13 10:35:32 +0800

    家具管理系统

--- c21/src/Add.java
+++ c21/src/Add.java
@@ -3,27 +3,125 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class Add {
-    //增加
-    public static void  add(int num, String name, String price, String productDate, String style) {
+    //增加 //如果家具已存在，则需要增加家具的数量，而不是再新增一条记录
+    public static void  add(String name, int price, int num, String dates, String style) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        //先根据家具的名称搜索数据库 是否已有该家具
+        try {
+            try{
+                connection = DB.getConnection();
+                String sql = "select id,num from Furnitures where name = ?;";
+                preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setString(1,name);
+                resultSet = preparedStatement.executeQuery();
+                if (resultSet != null) {
+                    //数据库无该家具 新增  //未能新增成功
+                    //写一个子函数试试
+//                    connection = DB.getConnection();
+//                    String sql2 = "insert into Furnitures( name, price, num,dates, style) values (?,?,?,?,?);";
+//                    preparedStatement = connection.prepareStatement(sql2);
+//                    preparedStatement.setString(1,name);
+//                    preparedStatement.setInt(2,price);
+//                    preparedStatement.setInt(3,num);
+//                    preparedStatement.setString(4,dates);
+//                    preparedStatement.setString(5,style);
+//                    System.out.println("insert success");
+                    sonAdd(name,price,num,dates,style);
+                } else {
+                    //数据库有该家具 修改数量
+                    int id = resultSet.getInt("id");
+                    int nums = resultSet.getInt("num");
+                    System.out.println("id" + id);
+//                    connection =DB.getConnection();
+//                    String sql3 = "update furnitures set num = ? where id = ?;";
+//                    preparedStatement = connection.prepareStatement(sql3);
+//                    preparedStatement.setInt(1,nums + num);
+//                    preparedStatement.setInt(2,id);
+//                    System.out.println("num insert success");
+                    sonMod(id,nums+num);
+                }
+            } finally {
+                DB.close(connection,preparedStatement,resultSet);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //有的话 num+1
+        //无的话 新增
+//        try {
+//            try{
+//                //创建数据库连接
+//                connection = DB.getConnection();
+//                String sql = "insert into Furnitures( name, price, num,dates, style) values (?,?,?,?,?);";
+//                //操作命令对象执行sql语句，返回结果集resultSet  resultSet类似List<map<String,Object>> String 指表头
+//                preparedStatement = connection.prepareStatement(sql);
+//                preparedStatement.setString(1,name);
+//                preparedStatement.setInt(2,price);
+//                preparedStatement.setInt(3,num);
+//                preparedStatement.setString(4,dates);
+//                preparedStatement.setString(5,style);
+//
+//                int rows = preparedStatement.executeUpdate(); //指示受影响的行数
+//                if (rows > 0) {
+//                    System.out.println("add success");
+//                }
+//            } finally {
+//                //释放资源 反向释放
+//                DB.close(connection,preparedStatement,resultSet);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    //新增子函数
+    public static void sonAdd(String name, int price, int num, String dates, String style) {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
         try {
             try{
-                //创建数据库连接
                 connection = DB.getConnection();
-                String sql = "insert into Furnitures(num, name, price, productDate, style) values (?,?,?,?,?);";
-                //操作命令对象执行sql语句，返回结果集resultSet  resultSet类似List<map<String,Object>> String 指表头
+                String sql = "insert into Furnitures( name, price, num,dates, style) values (?,?,?,?,?);";
                 preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setInt(1,num);
-                preparedStatement.setString(2,name);
-                preparedStatement.setString(3,price);
-                preparedStatement.setString(4,productDate);
+                preparedStatement.setString(1,name);
+                preparedStatement.setInt(2,price);
+                preparedStatement.setInt(3,num);
+                preparedStatement.setString(4,dates);
                 preparedStatement.setString(5,style);
 
                 int rows = preparedStatement.executeUpdate(); //指示受影响的行数
                 if (rows > 0) {
                     System.out.println("add success");
+                }
+            } finally {
+                //释放资源 反向释放
+                DB.close(connection,preparedStatement,resultSet);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //子函数 修改数量
+    public static void sonMod(int id, int num){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            try{
+                connection = DB.getConnection();
+                String sql = "update furnitures set num = ? where id = ?;";
+                preparedStatement = connection.prepareStatement(sql);
+
+                preparedStatement.setInt(1,num);
+                preparedStatement.setInt(2,id);
+
+                int rows = preparedStatement.executeUpdate(); //指示受影响的行数
+                if (rows > 0) {
+                    System.out.println("mod num  success");
                 }
             } finally {
                 //释放资源 反向释放
--- c21/src/Find.java
+++ c21/src/Find.java
@@ -4,6 +4,12 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Find {
+    //无法使用函数重载，因为参数类型和参数个数都一样
+
+    //根据家具的编号
+    //根据家具的价格
+    //根据家具的风格
+    //根据家具的数量
     //根据家具的名称来得到家具的全部信息
     public static void find(String namex) {
         Connection connection = null;
@@ -15,19 +21,21 @@ public class Find {
                 connection = DB.getConnection();
                 //根据数据库连接创建命令对象statement
                 //preparedStatement = connection.createStatement();
-                String sql = "select num, name, price, productDate, style from Furnitures where name = ?;";
+                String sql = "select id, name, price, num, dates,style from Furnitures where name = ?;";
                 //操作命令对象执行sql语句，返回结果集resultSet 只有一条记录
                 preparedStatement = connection.prepareStatement(sql);
                 preparedStatement.setString(1,namex);
                 resultSet = preparedStatement.executeQuery();
                 //assert preparedStatement != null;
                 while (resultSet.next()) {
-                    int num = resultSet.getInt("num"); //推荐字符串类型
+                    int id = resultSet.getInt("id"); //推荐字符串类型
                     String name = resultSet.getString("name");
                     String price = resultSet.getString("price");
-                    String productDate = resultSet.getString("productDate");
+                    String num = resultSet.getString("num");
+                    String dates = resultSet.getString("dates");
                     String style = resultSet.getString("style");
-                    System.out.printf("id = %s, name = %s, price = %s, productDate = %s, style = %s", num, name, price, productDate, style);
+                    System.out.printf("id = %d, name = %s, price = %d, num = %d, dates = %s, style = %s",
+                            id,name,price,num,dates,style);
                     System.out.println();
                 }
             } finally {
