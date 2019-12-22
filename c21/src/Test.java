8b1813f21d10614fe1981cff003e3bff76070db1
ZhouLan <1832311982@qq.com>
2019-12-22 21:34:12 +0800

    测试界面

--- c21/src/Test.java
+++ c21/src/Test.java
@@ -1,27 +1,28 @@
 public class Test {
     public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
         //增加家具 //如果家具已存在，则需要增加家具的数量，而不是再新增一条记录
-        Add.add("梳妆凳",500,50,"18-12-05","欧式风格");
-        Add.add("衣柜",600,20,"19-03-05","地中海风格");
-        Add.add("茶几",200,5,"18-11-06","现代前卫风格");
-        Add.add("儿童床",3000,18,"18-12-11","古典风格");
-        Add.add("书桌",550,60,"19-01-05","欧式风格");
-        Add.add("床",550,60,"19-01-05","欧式风格");
+//        Add.add("梳妆凳",500,50,"18-12-05","欧式风格");
+//        Add.add("衣柜",600,20,"19-03-05","地中海风格");
+//        Add.add("茶几",200,5,"18-11-06","现代前卫风格");
+//        Add.add("儿童床",3000,18,"18-12-11","古典风格");
+//        Add.add("书桌",550,60,"19-01-05","欧式风格");
+//        Add.add("床",550,60,"19-01-05","欧式风格");
 
 
         //根据家具的某个信息得到家具的全部信息
-        Find.findByName("床");
-        Find.findByName("茶几");
-        Find.findById(102);
-        Find.findByNum(200);
-        Find.findByPrice(5500);
-        Find.findByStyle("北欧风格");
+//        Find.findByName("床");
+//        Find.findByName("茶几");
+//        Find.findById(102);
+//        Find.findByNum(200);
+//        Find.findByPrice(5500);
+//        Find.findByStyle("北欧风格");
 
         //根据家具名称删除
-        Delete.delete("沙发");
-        Delete.delete("茶几");
+//        Delete.delete("沙发");
+//        Delete.delete("茶几");
 
         //根据家具名称修改 价格 数量 日期 风格
-        Update.update("床",8000,50,"2019-03-03","现代前卫风格");
+//        Update.update("床",8000,50,"2019-03-03","现代前卫风格");
     }
 }
