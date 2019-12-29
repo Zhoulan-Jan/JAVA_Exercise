99f92633be6f8c5871211ca3f396301f2f7aac4b
ZhouLan <1832311982@qq.com>
2019-12-29 13:30:02 +0800

    查询，新增了可返回的查询内容

--- c21/src/Find.java
+++ c21/src/Find.java
@@ -2,21 +2,22 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Find {
     //无法使用函数重载，因为参数类型和参数个数都一样
 
-    //根据家具的编号
-    public static void findById(int idx) {
+    //模糊查询，根据传入的风格
+    public static ArrayList<Furniture> findByStyle(String stylex) {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
+        ArrayList<Furniture> furs = new ArrayList<>();
         try {
             try{
                 connection = DB.getConnection();
-                String sql = "select id, name, price, num, dates,style from Furnitures where id = ?;";
+                String sql = "select id, name, price, num, dates,style from Furnitures where style like '%" + stylex + "%';";
                 preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setInt(1,idx);
                 resultSet = preparedStatement.executeQuery();
                 if (resultSet.next()) {
                     {   //找到的情况
@@ -27,6 +28,8 @@ public class Find {
                             int num = resultSet.getInt("num");
                             String dates = resultSet.getString("dates");
                             String style = resultSet.getString("style");
+                            Furniture f = new Furniture(id, name, price, num, dates, style);
+                            furs.add(f);
                             System.out.printf("id = %d, name = %s, price = %d, num = %d, dates = %s, style = %s",
                                     id,name,price,num,dates,style);
                             System.out.println();
@@ -42,10 +45,12 @@ public class Find {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return furs;
     }
 
     //根据家具的名称来得到家具的全部信息
-    public static void findByName(String namex) {
+    public static ArrayList<Furniture> findByName(String namex) {
+        ArrayList<Furniture> furs = new ArrayList<>();
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
@@ -58,6 +63,47 @@ public class Find {
                 resultSet = preparedStatement.executeQuery();
                 if (resultSet.next()) {
                      {   //找到的情况
+                        do {
+                            int id = resultSet.getInt("id"); //推荐字符串类型
+                            String name = resultSet.getString("name");
+                            int price = resultSet.getInt("price");
+                            int num = resultSet.getInt("num");
+                            String dates = resultSet.getString("dates");
+                            String style = resultSet.getString("style");
+                            Furniture f = new Furniture(id, name, price, num, dates, style);
+                            furs.add(f);
+                            System.out.printf("id = %d, name = %s, price = %d, num = %d, dates = %s, style = %s",
+                                    id,name,price,num,dates,style);
+                            System.out.println();
+                        } while (resultSet.next());
+                    }
+                } else {
+                    System.out.println("查找失败");
+                }
+            } finally {
+                //释放资源 反向释放
+                DB.close(connection,preparedStatement,resultSet);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return furs;
+    }
+
+    //根据家具的编号
+    public static void findById(int idx) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            try{
+                connection = DB.getConnection();
+                String sql = "select id, name, price, num, dates,style from Furnitures where id = ?;";
+                preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setInt(1,idx);
+                resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    {   //找到的情况
                         do {
                             int id = resultSet.getInt("id"); //推荐字符串类型
                             String name = resultSet.getString("name");
@@ -121,43 +167,8 @@ public class Find {
     }
 
 
-    //根据家具的风格
-    public static void findByStyle(String stylex) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            try{
-                connection = DB.getConnection();
-                String sql = "select id, name, price, num, dates,style from Furnitures where style = ?;";
-                preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setString(1,stylex);
-                resultSet = preparedStatement.executeQuery();
-                if (resultSet.next()) {
-                    {   //找到的情况
-                        do {
-                            int id = resultSet.getInt("id"); //推荐字符串类型
-                            String name = resultSet.getString("name");
-                            int price = resultSet.getInt("price");
-                            int num = resultSet.getInt("num");
-                            String dates = resultSet.getString("dates");
-                            String style = resultSet.getString("style");
-                            System.out.printf("id = %d, name = %s, price = %d, num = %d, dates = %s, style = %s",
-                                    id,name,price,num,dates,style);
-                            System.out.println();
-                        } while (resultSet.next());
-                    }
-                } else {
-                    System.out.println("查找失败");
-                }
-            } finally {
-                //释放资源 反向释放
-                DB.close(connection,preparedStatement,resultSet);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    //根据家具的风格 上移
+
 
     //根据家具的数量
     public static void findByNum(int numx) {
