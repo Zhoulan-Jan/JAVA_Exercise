abc9e1aba91c27c3a9e38209726372ce3ac6a344
ZhouLan <1832311982@qq.com>
2019-12-29 13:31:09 +0800

    显示，新增了可根据查询得到的内容展示

--- c21/src/Show.java
+++ c21/src/Show.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class Show {
     //根据数据库内容显示在表格上
@@ -41,4 +42,48 @@ public class Show {
         }
         return value;
     }
-}
+
+    //根据传入的Arraylist显示
+    public static String[][] show2(ArrayList<Furniture> furs) {
+        String[][] value  = new String[60][6];
+        int i = 0;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            try {
+                connection = DB.getConnection();
+                String sql = "select * from Furnitures where id = ?;";
+
+                for (int j = 0; j < furs.size(); j++) {
+                    preparedStatement = connection.prepareStatement(sql);
+                    preparedStatement.setInt(1, furs.get(i).getId());
+                    resultSet = preparedStatement.executeQuery();
+                    if (resultSet.next()) {
+                        {   //找到的情况
+                            do {
+                                //放在字符串数组里
+                                value[i][0] = String.valueOf(resultSet.getInt("id"));
+                                value[i][1] = resultSet.getString("name");
+                                value[i][2] = String.valueOf(resultSet.getInt("price"));
+                                value[i][3] = String.valueOf(resultSet.getInt("num"));
+                                value[i][4] = resultSet.getString("dates");
+                                value[i][5] = resultSet.getString("style");
+                                i++;
+                            } while (resultSet.next());
+                        }
+                    } else {
+                        System.out.println("查找失败");
+                    }
+                }
+            }finally {
+                //释放资源 反向释放
+                DB.close(connection,preparedStatement,resultSet);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return value;
+        }
+    }
+
